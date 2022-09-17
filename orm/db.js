2742c5eed79429f0d0cce7ddc7eb7738981fd009
aravindame <aravinda.mailbox@gmail.com>
2022-09-17 19:55:41 +0530

    mongo integrations

--- orm/db.js
+++ orm/db.js
@@ -1,12 +1,12 @@
 import Mongoose from "mongoose";
 
-export default function connect(){
-    const host = 'mongodb://mongo_example:27017/playground';
+var Course = null;
 
+export default function connect(){
+    const host = 'mongodb://localhost/playground';
     Mongoose.connect(host)
         .then(()=>console.log('Connection success.....'))
         .catch((error)=>console.log(error));
-        console.log(Mongoose.version)
     const schema = createSchema();
     createModel(schema)
 }
@@ -17,20 +17,43 @@ export function createSchema() {
         author: String,
         tags: [String],
         date: { type: Date, default: Date.now },
-        isPublished: Boolean
+        isPublished: Boolean,
+        price: Number
     });
     return schema;
 }
 
 export async function createModel(schema) {
-    const Course = Mongoose.model('Course', schema);
+    Course = Mongoose.model('Course', schema);
     const course = new Course({
         name: 'Master Node.js',
         author: 'Mosh',
         tags: ['node', 'backend'],
-        isPublished: true
+        isPublished: true,
+        price: 16
     });
-   // await course.save();
+    await course.save();
+    getCourses();
 }
 
-// sudo docker run --name mongo_example -d mongo:3.6.2
+export async function getCourses(){
+    const result = await Course.find()
+    .or([{name: "Mosh"}, {isPublished: false}])
+    .limit(2)
+    .select({ name : 1, author: 1, price: 1})
+    .count();
+    updateCourse('632569f20d022e179ce0c57c') 
+}
+
+export async function updateCourse(id='632569f20d022e179ce0c57c'){
+    const course = await Course.findById(id);
+    if(!course) return;
+    course.set({
+        isPublished: true,
+        author: 'Another Author'
+    });
+    const result = await course.save(course);
+    const foo = await Course.find({ name : "Mosh"})
+    .select({ name : 1, author: 1, price: 1})
+    .count();
+}
