be1631e689c0815e590b9f3e8a19752a45d66731
aravindame <aravinda.mailbox@gmail.com>
2022-09-19 20:18:00 +0530

    User model added

---
+++ models/user.js
@@ -0,0 +1,37 @@
+import Joi from 'joi';
+import mongoose from 'mongoose';
+
+export const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  },
+  isAdmin: Boolean
+});
+
+export const User = mongoose.model('User', userSchema);
+
+export default function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
