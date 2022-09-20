e4ff6febd055393de12007b91b899893e1ad21e5
aravindame <aravinda.mailbox@gmail.com>
2022-09-20 12:53:29 +0530

    index.js refactored.

--- index.js
+++ index.js
@@ -9,25 +9,44 @@ import users from './routes/users.js';
 import auth from './routes/auth.js';
 import { errorHandler } from './middleware/error-handler.js';
 
-winston.add(winston.transports.File, {filename: 'retina.log'});
-process.on('uncaughtException', (ex)=> winston.log(ex.message, ex));
-process.on('unhandledRejection', (ex)=> winston.log(ex.message, ex))
-
 const app = express();
 
-mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(errorHandler);
+(() => {
+  initLogger();
+  initUncaughtExceptionHandler();
+  dbconnection();
+  applyMiddlewares();
+  startServer();
+})();
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+function initLogger() {
+  winston.add(winston.transports.File, { filename: 'retina.log' });
+}
+
+function initUncaughtExceptionHandler() {
+  process.on('uncaughtException', (ex) => winston.log(ex.message, ex));
+  process.on('unhandledRejection', (ex) => winston.log(ex.message, ex));
+}
+
+function dbconnection() {
+  mongoose.connect('mongodb://localhost/vidly')
+  .then(() => winston.log('Connected to MongoDB...'))
+    .catch(err => winston.log(err.message, err));
+}
+
+function applyMiddlewares() {
+  app.use(express.json());
+  app.use('/api/genres', genres);
+  app.use('/api/customers', customers);
+  app.use('/api/movies', movies);
+  app.use('/api/rentals', rentals);
+  app.use('/api/users', users);
+  app.use('/api/auth', auth);
+  app.use(errorHandler);
+}
+
+function startServer() {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => winston.log(`Listening on port ${port}...`));
+}
